ad77dcf744f3c589db1329f3d443da717a1fcb17
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2022-05-10 00:55:28 +0300

    Initial commit

---
+++ src/js/libs/part-settings.js
@@ -0,0 +1,44 @@
+particlesJS('particles-js',
+{
+  "particles": {
+    "number": {
+      "value": 160,
+      "density": {
+        "enable": true,
+        "value_area": 1500
+      }
+    },
+    "size": {
+      "value": 1
+    },
+    "opacity": {
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0.05
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "opacity": 0.03
+    },
+    "move": {
+      "speed": 0.05,
+      "direction": "right"
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      }
+    },
+    "modes": {
+      "push": {
+        "particles_nb": 1
+      }
+    }
+  },
+  "retina_detect": true
+})
